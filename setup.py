8facc375d9bfa307ab812e436f3708c312c4ec83
Yessine Ben Rhouma <ben.rhouma.yessine0610@gmail.com>
2024-12-24 14:14:42 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="djangocms-seo",
-    version="0.1.0",
+    version="0.1.1",
     description="A Django CMS app for managing SEO.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
